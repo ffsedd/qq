a5ddd87c9792eacaf6b65b6a5b3131a647a2ee5a
ffsedd <ffsedd@gmail.com>
2019-08-29 11:39:51 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name='qq',
+    version='1.0.0',
+    url='https://github.com/ffsedd/qq',
+    author='ffsedd',
+    author_email='ffsedd@gmail.com',
+    description='python tools library',
+    packages=find_packages(),    
+    install_requires=['send2trash', 'pillow'],
+)
