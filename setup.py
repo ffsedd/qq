347165fa3ed5997a71efc0bfe4b051da26271918
ffsedd <ffsedd@gmail.com>
2019-09-02 10:52:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(
     name='qq',
     version='1.0.0',
-    url='https://github.com/ffsedd/qq.git',
+    url='https://github.com/ffsedd/qq/',
     author='ffsedd',
     author_email='ffsedd@gmail.com',
     description='python tools library',
