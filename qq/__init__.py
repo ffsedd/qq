2d105438589caa930f5f7b9fcf1e6a7211575d27
ffsed <ffsed@gmail.com>
2019-10-21 20:01:05 +0200

    6

--- qq/__init__.py
+++ qq/__init__.py
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 from .version import __version__
 from qq import datapro
 from qq import nptools
 from qq import npimage
 
 
-# if somebody does "from somepackage import *", this is what they will
+# if somebody does "from qq import *", this is what they will
 # be able to access:
 __all__ = [
     'datapro',
 ]
-=======
-import datapro
->>>>>>> d17f79f5e8f930a89b67ea2e9ecb1f953cf3e973
