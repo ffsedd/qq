cf80bd4d90316cec71d5f442badf025e0fa32307
ffsedd <ffsedd@gmail.com>
2019-12-01 08:58:57 +0100

    create

--- qq/__init__.py
+++ qq/__init__.py
@@ -1,10 +1,10 @@
 # from .version import __version__
-from qq import nptools
-from qq import npimage
-from qq import npfilters
-from qq import ostools
-from qq import misc
-from qq import terminaltools
+#from qq import nptools
+#from qq import npimage
+#from qq import npfilters
+#from qq import ostools
+#from qq import misc
+#from qq import terminaltools
 
 
 # if somebody does "from qq import *", this is what they will
