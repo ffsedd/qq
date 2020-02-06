1ba52540168e5622794c73f1ca7403bced938e21
ffsedd <ffsedd@gmail.com>
2020-02-06 18:19:02 +0100

    77

--- qq/npimage.py
+++ qq/npimage.py
@@ -514,9 +514,7 @@ def save_image(im, fp_out, bitdepth=None):
     
     # numpy image
     elif isinstance(im, np.ndarray):
-        print(fp.suffix.lower)
-        if fp.suffix.lower in [".jpg",".jpeg"]:
-            
+        if fp.suffix.lower() in [".jpg",".jpeg"]:
             numpy_to_jpg(im, fp)
             
         if not bitdepth:
